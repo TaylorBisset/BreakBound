15c6ef1e9ebead66f74e9212b754f2ef76321ccc
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-04-05 13:35:16 -0700

    timer.js created pauseTimer

--- src/timer.js
+++ src/timer.js
@@ -2,19 +2,20 @@
 
 // variables 
 let startTime = 0;
+let pausedTime = 0;
+let isPaused = false;
 let timeInterval = null;
 
 function startTimer() {
-    startTime = Date.now();
+    startTime = Date.now() - pausedTime;
     timeInterval = setInterval(updateTimer, 1000) // update every 1000 milliseconds
+    isPaused = false;
 }
 
 function pauseTimer() {
-
-}
-
-function stopTimer() {
-
+    clearInterval(timeInterval);
+    pausedTime = getTime();
+    isPaused = true;
 }
 
 function resetTimer() {
@@ -26,7 +27,7 @@ function updateTimer() {
 }
 
 function getTime() {
-
+    return Date.now() - startTime;
 }
 
 function convertTimeToHHMMSS() {
