d6a373af1d972f2c275682b4b46bb65bd9e66384
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-04-05 13:16:33 -0700

    timer.js set up functions

--- src/timer.js
+++ src/timer.js
@@ -1,5 +1,61 @@
 // timer.js
 
+// variables 
+let startTime = 0;
+let timeInterval = null;
+
+function startTimer() {
+    
+}
+
+function pauseTimer() {
+
+}
+
+function stopTimer() {
+
+}
+
+function resetTimer() {
+
+}
+
+function updateTimer() {
+
+}
+
+function getTime() {
+
+}
+
+function convertTimeToHHMMSS() {
+
+}
+
+function displayTime() {
+    
+}
+
+
 /*
+define variables
+
+startTime
+timeInterval
+
+
+define functions 
+
+startTimer
+pauseTimer
+stopTimer
+resetTimer
+updateTimer
+
+getTime
+convert time to HH:MM:SS
+updateTime
+displayTime
+
 
 */
