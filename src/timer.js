532867169504f48044e9fae7f03ae1f416fc271a
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-04-05 13:44:27 -0700

    timer.js updateTimer built

--- src/timer.js
+++ src/timer.js
@@ -1,10 +1,17 @@
 // timer.js
 
-// variables 
+// VARIABLES 
+
 let startTime = 0;
 let pausedTime = 0;
 let isPaused = false;
 let timeInterval = null;
+
+// FUNCTIONS 
+
+function getTime() {
+    return Date.now() - startTime;
+}
 
 function startTimer() {
     startTime = Date.now() - pausedTime;
@@ -13,24 +20,22 @@ function startTimer() {
 }
 
 function pauseTimer() {
-    clearInterval(timeInterval);
-    pausedTime = getTime();
+    clearInterval(timeInterval);    // Stops the timer
+    pausedTime = getTime();         // Store current time
     isPaused = true;
 }
 
-function resetTimer() {
-
-}
-
 function updateTimer() {
-
-}
-
-function getTime() {
-    return Date.now() - startTime;
+    let elapsedTime = getTime();
+    let formattedTime = convertTimeToHHMMSS(elapsedTime);
+    displayTime(formattedTime);
 }
 
 function convertTimeToHHMMSS() {
+
+}
+
+function resetTimer() {
 
 }
 
